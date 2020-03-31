72de072ef7d7f09368a85df1507cf0d5b67df961
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2020-03-31 17:22:06 +0300

    last RealAI

--- SQLite_example/RealAI.cpp
+++ SQLite_example/RealAI.cpp
@@ -9,18 +9,11 @@
 int fitness[7];
 int poz = 0;
 
-static int callback(void *data, int argc, char **argv, char **azColName) {
-	int i;
-	for (i = 0; i < argc; i++) {
-		fitness[poz++] = atoi(argv[i]);
-	}
-	return 0;
-}
-
-
 int raspunsAI(sqlite3* db, int argc, char** argv);
 void selectAnswers(sqlite3* db, int blackCardId, int* whiteCardId);
-
+static int callback(void *data, int argc, char **argv, char **azColName);
+int wheelOfFortune();
+int select(int* wheel);
 
 int main(int argc, char* argv[]) {
 	sqlite3 *db; 
@@ -41,7 +34,7 @@ int main(int argc, char* argv[]) {
 }
 
 int select(int* wheel) {
-	//TODO: find a diffrent random
+	//TODO: find a different random
 	srand(time(0));
 	int x = rand() % wheel[6];
 	for (int i = 0; i < 7; i++) {
@@ -60,6 +53,26 @@ int wheelOfFortune() {
 
 }
 
+static int callback(void *data, int argc, char **argv, char **azColName) {
+	int i;
+	for (i = 0; i < argc; i++) {
+		fitness[poz++] = atoi(argv[i]);
+	}
+	return 0;
+}
+
+void selectAnswers(sqlite3* db, int blackCardId, int* whiteCardId) {
+	char *zErrMsg = 0;
+	int rc;
+	char sql[1000];
+	for (int i = 0; i < 7; i++) {
+		//sprintf(sql, "SELECT valoare FROM relatie WHERE id_black = %d AND id_white = %d", blackCardId, whiteCardId[i]);
+		//cea de sus ar trebuii sa fie interogarea corecta, pe moment nu poate fi folosita din pricina inexistentei tabelului
+		sprintf(sql, "select id from raspunsuri where id = %d", i + 1);
+		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+	}
+}
+
 int raspunsAI(sqlite3* db, int argc, char** argv) {
 	//TODO: check if argc & argv are valid
 	int blackCardId = atoi(argv[2]);
@@ -69,16 +82,4 @@ int raspunsAI(sqlite3* db, int argc, char** argv) {
 	selectAnswers(db, blackCardId, whiteCardId);
 	int var = wheelOfFortune();
 	return whiteCardId[var];
-}
-void selectAnswers(sqlite3* db, int blackCardId, int* whiteCardId) {
-	char *zErrMsg = 0;
-	int rc;
-	char sql[1000];
-	for (int i = 0; i < 7; i++) {
-		//sprintf(sql, "SELECT valoare FROM relatie WHERE id_black = %d AND id_white = %d", blackCardId, whiteCardId[i]);
-		//cea de sus ar trebuii sa fie interogarea corecta, pe moment nu poate fi folosita din pricina inexistentei tabelului
-		sprintf(sql, "select id from raspunsuri where id = %d", i+1);
-		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-	}
-
 }
