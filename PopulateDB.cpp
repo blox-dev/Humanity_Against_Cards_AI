c8d5221e480919646c84fe67e7aa011aa3bae482
Olteanu Dragos <54898936+infoROM2000@users.noreply.github.com>
2020-04-01 08:53:21 +0300

    Update PopulateDB.cpp
    Fara comentarii

--- PopulateDB.cpp
+++ PopulateDB.cpp
@@ -1,23 +1,8 @@
-// PopulateDB.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
-#define _CRT_SECURE_NO_WARNINGS 1
-/*
-#include "sqlite3.h" 
-public void insert(JsonObject jsonObject) {
-    ContentValues values = new ContentValues();
-    SQLiteDatabase db = this.getWritableDatabase();
-    values.put("shopName", jsonObject.getString('Hello World'));
-    values.put("shopTeluguName", jsonObject.getString('shopTeluguName'));
-    values.put("shopAddress", jsonObject.getString('shopAddress'));
-    values.put("previousDues", jsonObject.getString('previousDues'));
-    values.put("shopID", jsonObject.getString('shopID'));
+#include "sqlite3.c"
+#include "sqlite3.h"
 
-    db.insert("YOUR TABLE NAME", null, values);
-}
-*/
 int aparitii(char* linie, char ch) {
     int count = 0;
     int i = 0;
@@ -29,7 +14,6 @@ int aparitii(char* linie, char ch) {
 }
 int main()
 {
-    /*
     CREATE TABLE[IF NOT EXISTS] Blacks(
         text TEXT NOT NULL,
         pick INTEGER CHECK(pick>0)
@@ -37,34 +21,21 @@ int main()
     CREATE TABLE[IF NOT EXISTS] Whites(
         text TEXT NOT NULL
     );
-    */
+    
     FILE* file;
     char linie[256];
     fopen_s(&file,"C:/Users/Eu/Cards.txt", "r");
         if (file != NULL)
         {
             while (fgets(linie, 256, file) != NULL) {
-                printf("Am citit %s", linie);
                 if (strchr(linie, '?') != NULL) //BLACK cu intrebare
-                    printf("1\n");//INSERT INTO Blacks VALUES(linie, 1); 
+                    INSERT INTO Blacks VALUES(linie, 1); 
                 else if (strchr(linie, '_') != NULL) //BLACK cu spatii
-                    printf("%d\n", aparitii(linie, '_')); //INSERT INTO Blacks VALUES(linie, aparitii(linie, '_');
+                    INSERT INTO Blacks VALUES(linie, aparitii(linie, '_');
                 else
-                        printf("3\n");//    INSERT INTO Whites VALUES(linie);
-                printf("\n");
+                    INSERT INTO Whites VALUES(linie);
             }
             fclose(file);
         }
         return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
