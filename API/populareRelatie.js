2d7fb9296b329fa9729f391f4a95f08636637a03
blox-dev <bloxorz51gaming@gmail.com>
2020-04-10 13:23:21 +0200

    Populare tabel de relatie

--- API/populareRelatie.js
+++ API/populareRelatie.js
@@ -1,33 +1,44 @@
-
-var MongoClient = require('mongodb').MongoClient; //necesar pentru a accepta node un client Mongo
+const MongoClient = require('mongodb').MongoClient; //necesar pentru a accepta node un client Mongo
 const url = "mongodb+srv://fluffypanda:thefluffa5@humanityagainstcards-vfnzh.gcp.mongodb.net/test?retryWrites=true&w=majority"; //endpoint-ul de la Mongo; e necesar pentru conectare
-var whiteCards;
-var blackCards;
 
-    MongoClient.connect(url,async function(err, db) { 
-    if (err) throw err;
-    var dbo = db.db("HumansAgainstCards"); 
-    
-    whiteCards = await dbo.collection("white_cards").find({}).toArray();
+async function main(){
+	const client = await MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+	var dbo = client.db("HumansAgainstCards"); //dbo va pointa aici specific la baza de date din ""; in Mongo mai sunt si admin si inca una si este necesar sa selectam fix baza asta
+ 	
+	whiteCards = await dbo.collection("white_cards").find({}).toArray();
     blackCards = await dbo.collection("black_cards").find({}).toArray();
+	
+	
+	//pentru resetarea tabelului
 
-    //inserarea campurilor blackCardId si whiteCardId in colectia blackcard_whitecard_relation
-    for(var i = 0; i < blackCards.length; i++){
+    //await dbo.collection("blackcard_whitecard_relation").drop();
+
+    /*
+    console.log("Collection dropped");
+
+    await dbo.createCollection("blackcard_whitecard_relation");
+
+    console.log("Collection created");
+    */
+	var k=0;
+	for(var i = 0; i < blackCards.length; i++){
         for (var j = 0; j < whiteCards.length; j++) {
-            var myobj = { blackCardId: blackCards[i]._id, whiteCardId: whiteCards[j]._id };
-            dbo.collection("blackcard_whitecard_relation").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            });
-        }
+			var myobj = { _id:k++, blackCardId: blackCards[i]._id, whiteCardId: whiteCards[j]._id, value: Random(), category:""};
+			await dbo.collection("blackcard_whitecard_relation").insertOne(myobj);
+	    }
     }
 
-    //echivalent cu select * from blackcard_whitecard_relation
-    await dbo.collection("blackcard_whitecard_relation").find({}).toArray(function(err,collections){ 
-    if (err) throw err; 
-    console.log(collections);
-    });
+    var count = await dbo.collection("blackcard_whitecard_relation").countDocuments();
+    
+    console.log(count);
 
- db.close();
+    await dbo.close();
+    await client.close();
+}
 
-});
+function Random(){
+	return Math.floor(Math.random()*10+10);
+}
+
+main().catch(console.error);
